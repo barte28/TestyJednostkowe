d272a480b4fd9b237ca0ac0b2eb742edf03d6f4c
barte28 <bartek.teuerle@gmail.com>
2025-09-04 08:30:15 +0200

    pierwszy

---
+++ src/Szyfry.java
@@ -0,0 +1,53 @@
+public class Szyfry {
+
+    public static String szyfrKwadratowy(String slowo, int klucz){
+        String zaszyfrowane = "";
+
+
+        return zaszyfrowane;
+
+    }
+
+    public static String szyfrCezara(String slowo,int klucz){
+        String zaszyfrowane = "";
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //przesunięcie alfabet
+        String alfabetPrzesuniety = "";
+        if(klucz>0) {
+            klucz = klucz % 26;
+
+            alfabetPrzesuniety = alfabet.substring(klucz, alfabet.length()) + alfabet.substring(0, klucz);
+        }
+        else {
+            klucz = klucz*-1;
+            klucz = klucz % 26;
+
+            alfabetPrzesuniety = alfabet.substring(alfabet.length()-klucz,alfabet.length()) + alfabet.substring(0, alfabet.length()-klucz);
+        }
+        int kod = 0;
+        for (int i = 0; i < slowo.length(); i++) {
+            kod = alfabet.indexOf(slowo.charAt(i));
+            zaszyfrowane = zaszyfrowane + alfabetPrzesuniety.charAt(kod);
+        }
+
+        return zaszyfrowane;
+    }
+
+    public static String szyfrCezaraAscii(String slowo, int klucz){
+        String zaszyfrowane = "";
+        klucz = klucz %26;
+        for (int i = 0; i < slowo.length(); i++) {
+            int kodLitery = (int)slowo.charAt(i)+klucz;
+            if (kodLitery>(int)'Z'){
+                kodLitery = kodLitery - 26;
+            }
+            if (kodLitery<(int)'A'){
+                kodLitery = kodLitery + 26;
+            }
+            char litera = (char)kodLitery;
+            zaszyfrowane = zaszyfrowane+litera;
+        }
+        return zaszyfrowane;
+    }
+
+}
---
+++ src/SzyfryTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SzyfryTest {
+
+    //Szyfr Cezara
+    @Test
+    void szyfrCezaraJezeliKlucz0() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezara("ABC",0));
+    }
+
+    @Test
+    void szyfrCezaraJezeliKluczMniejszodDlugosciAlfabetu() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezara("ABC",3));
+    }
+
+    @Test
+    void szyfrCezaraJezeliKluczWiekszyNizDlugoscAlfabetu() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezara("ABC",81));
+    }
+
+    @Test
+    void szyfrCezaraJezeliKluczUjemny() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezara("DEF",-3));
+    }
+
+    @Test
+    void szyfrCezaraJezeliBezZawijaniaWierszyKlucz3() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezara("ABC",3));
+    }
+
+    @Test
+    void szyfrCezaraJezeliZZawijaniemWierszyKlucz3() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezara("XYZ",3));
+    }
+
+
+    //Szyfr Cezara Ascii
+    @Test
+    void szyfrCezaraAsciiJezeliKlucz0() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezaraAscii("ABC",0));
+    }
+
+    @Test
+    void szyfrCezaraAsciiJezeliKluczMniejszodDlugosciAlfabetu() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezaraAscii("ABC",3));
+    }
+
+    @Test
+    void szyfrCezaraAsciiJezeliKluczWiekszyNizDlugoscAlfabetu() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezaraAscii("ABC",81));
+    }
+
+    @Test
+    void szyfrCezaraAsciiJezeliKluczUjemny() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezaraAscii("DEF",-3));
+    }
+
+    @Test
+    void szyfrCezaraAsciiJezeliBezZawijaniaWierszyKlucz3() {
+        Assertions.assertEquals("DEF",Szyfry.szyfrCezaraAscii("ABC",3));
+    }
+
+    @Test
+    void szyfrCezaraAsciiJezeliZZawijaniemWierszyKlucz3() {
+        Assertions.assertEquals("ABC",Szyfry.szyfrCezaraAscii("XYZ",3));
+    }
+
+    //szyfr Kwadratowy
+    @Test
+    void szyfrKwadratowyJezeliJestIdealnyKwadrat(){
+        Assertions.assertEquals("",Szyfry.szyfrKwadratowy("",));
+    }
+
+    @Test
+    void szyfrKwadratowy(){
+        Assertions.assertEquals("",Szyfry.szyfrKwadratowy("",));
+    }
+
+    @Test
+    void szyfrKwadratowy(){
+        Assertions.assertEquals("",Szyfry.szyfrKwadratowy("",));
+    }
+
+}
